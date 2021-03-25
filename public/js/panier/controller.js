25a97f111663e9fe42aefa80c8600e150530ea65
SalsabilHamda <lecomptedesalsa@gmail.com>
2021-03-25 15:15:55 +0100

    first

---
+++ public/js/panier/controller.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    
+    
+    
+    
+    
+    
+    vider.addEventListener('click' (event)=>{
+            event.preventDefault;
+            let p = new Panier;
+            p.addOne(btn);
+        })
+    
+    
+});
---
+++ public/js/panier/product.js
@@ -0,0 +1,7 @@
+class Product{
+
+    addProduct(id){
+
+        fetch()
+    }
+}
